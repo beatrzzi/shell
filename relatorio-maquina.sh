8986bf64851646c86f6ca88d5fb84e308bbe0598
Beatriz Miranda <82675285+beatrzzi@users.noreply.github.com>
2022-04-05 15:41:54 -0300

    Update relatorio-maquina.sh

--- relatorio-maquina.sh
+++ relatorio-maquina.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 ###################################################
-#exercicio 2 do cap 3 do curso de shell script
 #autor: Beatriz Miranda
 #data: 05/04/2022
 #o script gera um relatório da máquina contendo:
@@ -22,11 +21,14 @@ KERNEL=$(uname -r) # versao do kernel
 
 HOSTNAME=$(hostname) #hostname da maquina
 
-NUMCPU=$(cat /proc/cpuinfo |grep "model name"|wc -l) #o cat procura dentro de /proc/cpuinfo as informacoes referentes a "model name,e depois o wc -l printa quantas linhas foram retornada, que nesse caso corresponde a quantidade de cpu ativas
+NUMCPU=$(cat /proc/cpuinfo |grep "model name"|wc -l) 
+#o cat procura dentro de /proc/cpuinfo as informacoes referentes a "model name,e depois o wc -l printa quantas linhas foram retornada, que nesse caso corresponde a quantidade de cpu ativas
 
-CPUMODEL=$(cat /proc/cpuinfo |grep "model name"|head -n1|cut -c14-) #o cat procura dentro de /proc/cpuinfo as informacoes referentes a "model name" e com o head -n1 peço para que seja mostrada apenas a primeira linha de resultado. A partir disso, com o cut -c14- faço apenas um corte para retornar apenas o nome da cpu 
+CPUMODEL=$(cat /proc/cpuinfo |grep "model name"|head -n1|cut -c14-) 
+#o cat procura dentro de /proc/cpuinfo as informacoes referentes a "model name" e com o head -n1 peço para que seja mostrada apenas a primeira linha de resultado. A partir disso, com o cut -c14- faço apenas um corte para retornar apenas o nome da cpu 
 
-MEMTOTAL=$(expr $(cat /proc/meminfo |grep MemTotal|tr -d " "|cut -d: -f2|tr -d kB) / 1024) # retorna a memoria total da maquina em MB transformando o valor total em kB contido em MemTotal
+MEMTOTAL=$(expr $(cat /proc/meminfo |grep MemTotal|tr -d " "|cut -d: -f2|tr -d kB) / 1024) 
+#retorna a memoria total da maquina em MB transformando o valor total em kB contido em MemTotal
 
 FILESYSTEM=$(df -h|egrep -v '(tmpfs|udev|/dev/loop)') 
 
