96c2859787546b6b6f84ab12d37fd95774f15e49
Beatriz Miranda <b.mirandabeatriz@gmail.com>
2022-08-17 10:05:33 -0300

    Update pega_cartas_sinoticas.sh

--- pega_cartas_sinoticas.sh
+++ pega_cartas_sinoticas.sh
@@ -1,13 +1,19 @@
-!/bin/bash
+# autora: beatriz miranda
+# script para pegar cartas sinoticas diretamente dos sites das instituições
+# loop apenas no dia
+# uso: apos fazer o download do script, digite "chmod +x pega_cartas_sinoticas.sh" no seu terminal
+# para alterar o intervalo de dias basta modificar no script
 
+!/bin/bash
+#marinha: https://www.marinha.mil.br/chm/sites/www.marinha.mil.br.chm/files/cartas-sinoticas/c2208${dia}00.png
+#cptec: http://img0.cptec.inpe.br/~rgptimg/Produtos-Pagina/Carta-Sinotica/Analise/Altitude/altitude_201103${dia}00.gif
+
+#exemplo utilizando as cartas do cptec
 dia='12'
 
 while [ ${dia} -le 17 ]; do
-	wget -r -P /media/extra/ http://img0.cptec.inpe.br/~rgptimg/Produtos-Pagina/Carta-Sinotica/Analise/Altitude/altitude_201103${dia}00.gif
-
+	wget -r -P /diretorio_para_guardar_cartas/ http://img0.cptec.inpe.br/~rgptimg/Produtos-Pagina/Carta-Sinotica/Analise/Altitude/altitude_201103${dia}00.gif
+	
 let dia=dia+1;
 
 done
-~             
-#https://www.marinha.mil.br/chm/sites/www.marinha.mil.br.chm/files/cartas-sinoticas/c2208${dia}00.png
-
